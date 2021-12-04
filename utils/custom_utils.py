18d3561030a8f415b12d9e644e5fd52e28f077fe
lorepas <pascolorenzo@gmail.com>
2021-12-04 18:14:14 +0000

    fixed the metrics to calculate accuracy

--- utils/custom_utils.py
+++ utils/custom_utils.py
@@ -12,6 +12,8 @@ import sys
 from matplotlib import pyplot as plt
 from PIL import Image, ImageDraw, ImageShow, ImageFont
 from tqdm.notebook import tqdm as tqdm
+from operator import itemgetter
+from tabulate import tabulate
 from utils.engine import evaluate
 
 import torch
@@ -145,35 +147,122 @@ def visualize_from_tensor_and_bb(train_dataset, rl):
         img, target = train_dataset[i]
         visualize_from_tensor(img, target, category_id_to_name)
         
-def visualize_prediction(dataset, random_list, model, device, path=None):
-    for l in random_list:
+# def visualize_prediction(dataset, random_list, model, device, path=None):
+#     for l in random_list:
+#         img,target = dataset[l]
+#         im = Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy())
+#         model.eval()
+#         with torch.no_grad():
+#             prediction = model([img.to(device)])
+#         boxes = prediction[0]['boxes'].tolist()
+#         labels = prediction[0]['labels'].tolist()
+#         scores = prediction[0]['scores'].tolist()
+#         size = len(target['labels'])
+
+#         if len(labels) > 0:
+#             for i in range(size):
+#                 if labels[i] == 1:
+#                     color = 'green'
+#                     text = 'no fall'
+#                 elif labels[i] == 2:
+#                     color = 'red'
+#                     text = 'fall'
+#                 x0,y0,x1,y1 = boxes[i]
+
+#                 draw = ImageDraw.Draw(im)
+#                 draw.rectangle(((x0, y0),(x1,y1)), outline=color, width=3)
+#                 draw.text((x0, y0), text)
+#         if path is None:
+#             ImageShow.show(im)
+#         elif path == "pred_test_images" or path == "pred_virtual_images":
+#             #ImageShow.show(im)
+#             im.save(f'snapshots/{path}/pred_img_{l}.png')
+#         else:
+#             raise ValueError(f'save argument can be: None, pred_test_images or pred_virtual_images, not {path}')
+            
+# ---------------------NEW ACCURACY----------------------------------
+def load_saved_model(path):
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # create a Faster R-CNN model without pre-trained
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False, pretrained_backbone=False)
+
+    num_classes = 3 # wheat or not(background)
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+
+    # replace the pre-trained model's head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # load the trained weights
+    model.load_state_dict(torch.load(path, map_location=device))
+    model.eval()
+
+    # move model to the right device
+    _ = model.to(device)
+    return model, device
+
+def take_prediction(prediction, threshold):
+    boxes = prediction['boxes'].tolist()
+    labels = prediction['labels'].tolist()
+    scores = prediction['scores'].tolist()
+
+    if len(boxes) == 0:
+        return [[0,0,0,0],2,0.]
+
+    if all(l == 1 for l in labels) or all(l == 2 for l in labels):
+        if scores[0] < threshold:
+            return [[0,0,0,0],2,0.]
+        else:
+             return [boxes[0],labels[0],scores[0]]
+    
+    max_no_fall = max((t for t in zip(boxes,labels,scores) if t[1] == 1), key=itemgetter(2))
+    max_fall = max((t for t in zip(boxes,labels,scores) if t[1] == 2), key=itemgetter(2))
+    max_score = max(max_no_fall, max_fall, key=itemgetter(2))
+
+    if max_score[2] > threshold:
+        return max_score
+    return [[0,0,0,0],2,0.]
+
+def visualize_prediction(dataset, list_imgs, model, device, path=None, thr=0.7):
+    for l in list_imgs:
         img,target = dataset[l]
-        im = Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy())
-        model.eval()
         with torch.no_grad():
             prediction = model([img.to(device)])
-        boxes = prediction[0]['boxes'].tolist()
-        labels = prediction[0]['labels'].tolist()
-        scores = prediction[0]['scores'].tolist()
-        size = len(target['labels'])
-
-        if len(labels) > 0:
-            for i in range(size):
-                if labels[i] == 1:
-                    color = 'green'
-                    text = 'no fall'
-                elif labels[i] == 2:
-                    color = 'red'
-                    text = 'fall'
-                x0,y0,x1,y1 = boxes[i]
-
-                draw = ImageDraw.Draw(im)
-                draw.rectangle(((x0, y0),(x1,y1)), outline=color, width=3)
-                draw.text((x0, y0), text)
+        bb,label,score = take_prediction(prediction[0],thr)
+        if label == 1:
+            color = "green"
+            text = f"no fallen: {score:.3f}"
+        else:
+            color = "red"
+            text = f"fallen: {score:.3f}"
+        x0,y0,x1,y1 = bb
+        im = Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy())
+        draw = ImageDraw.Draw(im)
+        draw.rectangle(((x0, y0),(x1,y1)), outline=color, width=3)
+        draw.text((x0, y0), text)
         if path is None:
             ImageShow.show(im)
-        elif path == "pred_test_images" or path == "pred_virtual_images":
-            #ImageShow.show(im)
-            im.save(f'snapshots/{path}/pred_img_{l}.png')
         else:
-            raise ValueError(f'save argument can be: None, pred_test_images or pred_virtual_images, not {path}')
+            im.save(f'snapshots/{path}{l}.png')
+        
+def classifier_performance(dataset, model, device, tr = 0.7):
+    tn, tp, fn, fp = 0,0,0,0
+    for im,target in tqdm(dataset):
+        gt_labels = target['labels'].tolist()
+        with torch.no_grad():
+            prediction = model([im.to(device)])
+        _, label, _ = take_prediction(prediction[0], tr)
+        if label == 1: 
+            if label in gt_labels:
+                tn = tn + 1
+            else:
+                fn = fn + 1
+        elif label == 2:
+            if label in gt_labels:
+                tp = tp + 1        
+            else:
+                fp = fp + 1
+        accuracy = (tp+tn)/(tp+tn+fn+fp)
+    return accuracy, tn, tp, fn, fp
